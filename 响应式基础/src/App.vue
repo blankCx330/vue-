de0f7d45f5920ed9bf5c062131b60d073be68ce3
blankcx330 <2486119856@qq.com>
2025-09-27 17:19:49 +0800

    学习响应式基础

--- 响应式基础/src/App.vue
+++ 响应式基础/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>//在单文件组件 (SFC)中<script setup>可以自动暴露函数和变量
-import { ref, shallowRef, nextTick } from 'vue' //要记得引入ref
+import { ref, shallowRef, nextTick, reactive } from 'vue' //要记得引入ref
 const count = ref(0)//ref接受一个参数，然后返回一个ref对象
 const text = ref('学习响应式基础文本');
 const update = () => {
@@ -18,6 +18,8 @@ const updateObj1 = () =>{
   obj1.value.numObj += 42;
   obj1.value.arr.push(`ah!push${obj1.value.arr.length + 1}`);//push不会触发视图更新
 }
+
+
 
 // shallowRef创建一个浅响应式对象
 // 简单来说shallowRef只会追踪第一层的变化(shallowObj.value)
@@ -38,6 +40,8 @@ const updateShallowObj = () => {
   }
 }
 
+
+
 //DOM更新时机
 const domText = ref('初始文本');
 const divText = ref(null);//获取DOM元素的引用
@@ -48,6 +52,65 @@ async function changeDomText() {
   await nextTick();
   console.log('nextTick:', divText.value?.textContent);//修改后的文本
 }
+
+
+
+//reactive()将深层地转换对象
+//在ref()的值是一个对象时，ref()也会在内部调用它
+//与浅层 ref 类似，这里也有一个 shallowReactive() API 可以选择退出深层响应性。
+const raw = {};
+const obj = reactive(raw);
+//返回一个Proxy代理对象这个对象与raw不是全等的
+console.log(obj === raw);//false
+
+//对同一个原始对象调用 reactive() 会总是返回同样的代理对象
+//reactive对同一个对象多次调用会返回同一个响应式对象
+console.log(reactive(raw) === obj);//true
+
+//reactive对一个已经是响应式的对象调用会返回这个对象本身
+console.log(reactive(obj) === obj);//true
+
+//依靠深层响应性，响应式对象内的嵌套对象依然是代理：
+obj.b = raw;
+console.log(obj.b === raw);//false
+
+//修改obj会影响raw
+obj.a = 1;
+console.log(raw.a);
+
+//reactive()不能替换整个对象
+//简单来说就是当代理对象state执行state = reactive({ num: 1});时
+//state会等于一个新的{num: 1}的代理对象,失去了对{num: 0}的引用
+//state并未监视到{num: 0},{num: 1}的变化
+//因此视图不会更新
+let state = reactive({ stateNum: 0});
+const updateState = () => {
+  state = reactive({ stateNum: 1});//不会报错
+  console.log("state.stateNum:"+state.stateNum);//1
+}
+
+//reactive()对解构操作不友好
+//stateNum收到的是一个普通的数字
+// 并且无法追踪 state.count 的变化
+// 我们必须传入整个对象以保持响应性
+let { stateNum } = state;
+const updateStateNum = () => {
+  stateNum++;
+  console.log("stateNum:" + stateNum);
+}
+//鉴于以上reactive()的限制,vue官方推荐使用ref()作为声明响应式状态的主要 API。
+
+let tempObj = ref({ temp: 0});
+const updateTempObj = () => {
+  tempObj = ref({ temp: 1});
+  console.log(" tempObj:" + tempObj.value.temp);//记住rel要使用value来访问
+};
+
+//temp仍然不会追踪
+let {temp} = tempObj.value;
+console.log(temp);
+tempObj.value.temp = 5;
+console.log(temp);
 
 </script>
 <template>
@@ -67,8 +130,17 @@ async function changeDomText() {
   <!-- 会触发视图更新 -->
   <button @click="updateShallowObj">查看shallowObj</button>
 
+  <!-- DOM更新时机 -->
   <div ref="divText" >{{ domText }}</div>
   <button @click="changeDomText">修改domText</button>
+
+  <!-- 视图不会更新 -->
+  <div>state: {{ state }}</div>
+  <button @click="updateState">更新state</button>
+  <button @click="updateStateNum">StateNum++</button>
+
+  <div>tempObj: {{ tempObj }} temp: {{ temp }}</div>
+  <button @click="updateTempObj">更新tempObj</button>
 </template>
 
 <style scoped>
