bc1c7b621f29f807a0ea3ffc8394f9f81a1c8a41
blankcx330 <2486119856@qq.com>
2025-09-29 23:52:43 +0800

    学习Class与Style绑定

--- Class与Style绑定/src/App.vue
+++ Class与Style绑定/src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 const text = ref('这是文本');
 const isActive = ref(true);
+const  hasError = ref(false);
 const updateActive = () => {
   isActive.value = !isActive.value;
 };
@@ -9,16 +10,17 @@ const updateActive = () => {
 
 <template>
   <!-- active 是否存在取决于数据属性 isActive 的真假值 -->
+  <!-- 由vue框架实现 -->
   <div :class="{ active: isActive }">hello</div>
   <button @click="updateActive">Toggle Active</button>
 
   <!-- class可以与:class共存
    这里最后渲染的结果会是
    <div class="font-bold active">{{ text }}</div> -->
-   
+
   <div 
     class="font-bold"
-    :class="{ active: isActive }"
+    :class="{ active: isActive, 'font-szie': hasError }"
     >{{ text }}</div>
 </template>
 
@@ -28,5 +30,8 @@ const updateActive = () => {
 }
 .font-bold {
   font-weight: bold;
+}
+.font-size{
+  font-size: 2rem;
 }
 </style>
