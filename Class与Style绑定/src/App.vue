0d13121360d68169d5af0a94d6551f09d653c6e0
blankcx330 <2486119856@qq.com>
2025-09-30 23:50:23 +0800

    学习Class与Style绑定

--- Class与Style绑定/src/App.vue
+++ Class与Style绑定/src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 const text = ref('这是文本');
 const isActive = ref(true);
 const  hasError = ref(false);
 const updateActive = () => {
   isActive.value = !isActive.value;
 };
+const classObj = ref({
+  classObj1: true,
+  classObj2: false,
+})
+const classObjComputed = computed(() => ({
+  classObj1: isActive.value && !hasError.value,
+  classObj2: hasError.value || hasError.value === false,
+}))
 </script>
 
 <template>
@@ -17,11 +25,28 @@ const updateActive = () => {
   <!-- class可以与:class共存
    这里最后渲染的结果会是
    <div class="font-bold active">{{ text }}</div> -->
-
   <div 
     class="font-bold"
     :class="{ active: isActive, 'font-szie': hasError }"
     >{{ text }}</div>
+
+  <!-- 也可以直接用声明的对象
+   下面会渲染为
+   <div class="classObj1">{{ text }}</div> -->
+  <div :class="classObj">{{ text }}</div>
+
+  <!-- 也可以用计算属性
+   const classObjComputed = computed(() => ({
+  classObj1: isActive.value && !hasError.value,
+  classObj2: hasError.value || hasError.value === false,
+})) -->
+  <div :class="classObjComputed">{{ text }}</div>
+
+  <!-- 也可以数组形式 -->
+  <!-- 等价于<div class="font-bold classObj1">{{ text }}</div> -->
+  <div :class="['font-bold', classObj]">{{ text }}</div>
+
+  <div>{{ text }}</div>
 </template>
 
 <style scoped>
@@ -32,6 +57,12 @@ const updateActive = () => {
   font-weight: bold;
 }
 .font-size{
+  font-size: 2rem;
+}
+.classObj1{
+  color: blue;
+}
+.classObj2{
   font-size: 2rem;
 }
 </style>
