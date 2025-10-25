84fcb031401b82c94e8b66b48aba4930197ea77e
blankcx330 <2486119856@qq.com>
2025-10-25 22:47:09 +0800

    学习组件基础 导入.vue和.js文件

--- 组件基础/src/App.vue
+++ 组件基础/src/App.vue
@@ -1,10 +1,25 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
+//通过这个来引用别的组件
+import clickButton from './components/clickButton.vue';
+
+// 引用的.js文件中使用template需要配置构建工具支持模板编译
+// 在vite.config.js的 alias: {} 中加入以下配置
+// 'vue': 'vue/dist/vue.esm-bundler.js' // 关键配置
+import jsClickButton from './components/jsClickButton.js';
 const text = ref('这是组件基础文本')
+const jsButtonRef = ref(null);
+onMounted(()=>{
+  console.log(jsButtonRef.value);
+  
+})
 </script>
 
 <template>
   <h1>{{ text }}</h1>
+  <click-button />
+  <br />
+  <js-click-button ref="jsButtonRef" />
 </template>
 
 <style scoped></style>
---
+++ 组件基础/src/components/clickButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+// 当使用构建步骤时
+// 我们一般会将 Vue 组件定义在一个单独的 .vue 文件中
+// 这被叫做单文件组件 (简称 SFC)：
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+<button @click="count++">count:{{ count }}</button>
+
+</template>
+
+<style scoped>
+button{
+ font-size: 1.2rem
+}
+</style>
