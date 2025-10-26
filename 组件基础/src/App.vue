0716e4bdfcc168f967921c1a7c1beb99c100f7d4
blankcx330 <2486119856@qq.com>
2025-10-26 23:23:49 +0800

    学习组件基础

--- 组件基础/src/App.vue
+++ 组件基础/src/App.vue
@@ -17,9 +17,31 @@ onMounted(()=>{
 
 <template>
   <h1>{{ text }}</h1>
-  <click-button />
+  <!-- 
+  值得一提的是每个组件都是独立的
+  每一个组件都维护着自己的状态，是不同的 count
+  这是因为每当你使用一个组件，就创建了一个新的实例。 
+  -->
+  <!--
+    组件实例是 Vue 中最小可复用的功能单元
+    每个实例自带数据和方法，彼此独立又协同工作。
+  -->
+  <clickButton class="red"/>
+  <clickButton class="blue"/>
   <br />
-  <js-click-button ref="jsButtonRef" />
+  <jsClickButton ref="jsButtonRef" />
+  <!-- 在单文件组件中
+  推荐为子组件使用 PascalCase 的标签名
+  以此来和原生的 HTML 元素作区分。 -->
 </template>
 
-<style scoped></style>
+<style scoped>
+.red{
+  color: #fff;
+  background-color: red;
+}
+.blue{
+  color: #fff;
+  background-color: blue;
+}
+</style>
--- 组件基础/src/components/jsClickButton.js
+++ 组件基础/src/components/jsClickButton.js
@@ -1,12 +1,21 @@
 import {ref} from 'vue'
 
+// 这是一个用js来定义的组件
+// export default
+// 功能​：作为 JavaScript ES6 模块系统的标准语法
+// 用于导出模块的默认值（单个值）。
 export default{
+    // setup
+    // 在 Vue3 中替代 Vue2 的 data、methods等选项
+    // 用于组织组件逻辑
+    // 支持响应式数据、生命周期钩子、计算属性等。
     setup(){
         const count = ref(0)
         return { 
             count 
         }
     },
+    //下面的
     template: `
     <button @click="count++">
       JSbutton:{{ count }}
